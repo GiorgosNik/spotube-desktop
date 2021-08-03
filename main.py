16be00871c5c61f8f638a83184af7e019200b0d4
lybrag1 <58698711+lybrag1@users.noreply.github.com>
2021-08-03 11:53:28 +0300

    Improved Song Selection

--- main.py
+++ main.py
@@ -7,21 +7,28 @@ from spotipy.oauth2 import SpotifyClientCredentials
 import eyed3
 import requests
 import shutil
+import random
 
 # Set the Spotify API Keys
 CLIENT_ID = 'ff55dcadd44e4cb0819ebe5be80ab687'
 CLIENT_SECRET = '5539f7392ae94dd5b3dfc1d57381303a'
 
 # Set the downloader
-audio_downloader = YoutubeDL({'format':'bestaudio'})
-
+audio_downloader = YoutubeDL({'format': 'bestaudio'})
 
 # Auth with the Spotify Framework
 auth_manager = SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
 sp = spotipy.Spotify(auth_manager=auth_manager)
 
+# Set up the folder for the songs
+if not os.path.isdir('./Songs'):
+    os.mkdir('./Songs')
+else:
+    os.rename('./Songs', './OldSongs' + str(random.randrange(100)))
+    os.mkdir('./Songs')
+
 # Get data for every song in the playlist
-playlistToGet = sp.playlist('https://open.spotify.com/playlist/2tUdm8UGbwNQzxOtF2pHiu?si=3e63f30913d24542')
+playlistToGet = sp.playlist('https://open.spotify.com/playlist/0g7eTKPSN1IarlunWjnITk?si=25d79d887fa44775')
 for item in playlistToGet['tracks']['items']:
     song = item['track']
     cover_art = song['album']['images'][0]['url']
@@ -37,30 +44,30 @@ for item in playlistToGet['tracks']['items']:
     artist = artistList
     album = song['album']['name']
     duration = int(song['duration_ms'])
-
+    print("Searching for Name: ",name)
     # Search for the best candidate
     minDifference = duration
-    videoSearch = VideosSearch(name + " " + artist, limit=5)
+    videoSearch = VideosSearch(name + " " + artist, limit=3)
     for searchItem in videoSearch.result()['result']:
         durationStr = searchItem['duration'].replace(":", " ").split()
         durationInt = int(durationStr[0]) * 60000 + int(durationStr[1]) * 1000
         if abs(durationInt - duration) < minDifference:
-            duration = durationInt
+            minDifference = abs(durationInt - duration)
             link = searchItem['link']
-            
     print(name, artist, album, year, link)
 
     # Try to download the song
     try:
         URL = link
-        text=audio_downloader.extract_info(URL)
-        filename=text['title']+"-"+text["id"]+".webm"
+        text = audio_downloader.extract_info(URL)
+        filename = text['title'] + "-" + text["id"] + ".webm"
     except Exception:
         print("Couldn\'t download the audio")
-    filename=filename.replace('"', "'")
+    filename = filename.replace('"', "'")
     print(filename)
     webm_audio = AudioSegment.from_file(filename, format="webm")
-    webm_audio.export(name+".mp3", format="mp3")
+    webm_audio.export(name + ".mp3", format="mp3")
+    os.remove(filename)
 
     # Get the Cover Art
     image_url = cover_art
@@ -74,14 +81,16 @@ for item in playlistToGet['tracks']['items']:
     else:
         print('Image Couldn\'t be retrieved')
 
-    audiofile = eyed3.load(name+'.mp3')
-    if (audiofile.tag == None):
-        audiofile.initTag()
+    # Edit the ID3 Tags
+    audioFile = eyed3.load(name + '.mp3')
+    if audioFile.tag is None:
+        audioFile.initTag()
 
-    audiofile.tag.images.set(3, open('cover_photo.jpg', 'rb').read(), 'image/jpeg')
-    audiofile.tag.artist=artist
-    audiofile.tag.title=name
-    audiofile.tag.album=album
-    audiofile.tag.year=year
-    audiofile.tag.save()
+    audioFile.tag.images.set(3, open('cover_photo.jpg', 'rb').read(), 'image/jpeg')
+    audioFile.tag.artist = artist
+    audioFile.tag.title = name
+    audioFile.tag.album = album
+    audioFile.tag.year = year
+    audioFile.tag.save()
 
+    shutil.move('./' + name + '.mp3', './Songs/' + name + '.mp3')
